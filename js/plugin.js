92a42a47fbcc904e52f96cd43370eecd49b2dec5
AliriDesu <1183979251@qq.com>
2021-06-16 20:42:26 +0800

    Signed-off-by: AliriDesu <1183979251@qq.com>

--- js/plugin.js
+++ js/plugin.js
@@ -1,10 +1,12 @@
 var clientH = document.documentElement.clientHeight,
     clientW = document.documentElement.clientWidth,
     index = 0,
+    lIndex = -1,
     mainObj = document.getElementsByClassName('main')[0],
     timer1 = null,
     timer2 = null,
     t,
+    Y1, Y2,
     navs = document.getElementsByClassName('nav');
 
 function init() {
@@ -12,25 +14,35 @@ function init() {
     window.addEventListener('resize', function () {
         clientH = document.documentElement.clientHeight;
         clientW = document.documentElement.clientWidth;
-        
+
     });
     window.addEventListener('wheel', function (e) {
         if (!t || Date.now() - t >= 1000) {
-            console.log(88);
-            console.log(t);
             if (e.deltaY == 100) {
                 pageTo(1, undefined);
             } else if (e.deltaY == -100) {
-                pageTo(-1, undefined);  
+                pageTo(-1, undefined);
             }
             t = Date.now();
         }
-
-
-
     });
+
+    window.addEventListener('touchstart', function (e) {
+        Y1 = e.touches[0].clientY;
+    })
+    window.addEventListener('touchend', function (e) {
+        Y2 = e.changedTouches[0].clientY;
+        if (Y2 - Y1 > 0) {
+            pageTo(-1, undefined);
+        } else {
+            pageTo(1, undefined);
+        }
+    })
+
+
     for (let i = 0; i < navs.length; i++) {
         navs[i].addEventListener('click', function (e) {
+            lIndex = index;
             index = i;
             pageTo(undefined, index);
 
@@ -45,10 +57,11 @@ function init() {
 init();
 
 function pageTo(dir, to) {
+
     if (to != undefined) {
         mainObj.style.top = -to * clientH + 'px';
     } else {
-
+        lIndex = index;
         index = index + dir;
         if (index <= 0) {
             index = 0;
@@ -60,8 +73,24 @@ function pageTo(dir, to) {
         //     mainObj.style.top = '0px';
         // }
     }
-
+    if(lIndex==index){
+        return
+    }
     navColor();
+    if (index == 1) {
+
+        changeSkillBar(true)
+    } else {
+
+        changeSkillBar(false)
+    }
+    if (index == 3) {
+
+        showAddress()
+    } else {
+
+        hideAddress()
+    }
 }
 
 function navColor() {
