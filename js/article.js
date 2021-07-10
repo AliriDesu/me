55ab24d5a51cbf22c259a55efe3e4a2e7db88346
aliri <1183979251@qq.com>
2021-07-10 10:13:50 +0800

    cao

---
+++ js/article.js
@@ -0,0 +1,39 @@
+var articleList = document.getElementsByClassName('articleList')[0];
+console.log(articleList);
+var list = [{
+        title: '01-html标签',
+        url: '../note/1.html'
+    },
+    {
+        title: '02-html标签',
+        url: '../note/2.html'
+    },
+    {
+        title: '03-css基础1',
+        url: '../note/3.html'
+    }, 
+    {
+        title: '04-css基础2',
+        url: '../note/4.html'
+    },
+]
+
+
+
+
+
+
+
+
+
+for (let i = 0; i < list.length; i++) {
+    let a = document.createElement('a');
+    a.href = '' + list[i].url;
+    a.target = '_blank'
+    let p = document.createElement("p");
+    p.innerHTML = '' + list[i].title;
+    p.style.fontSize = '32px';
+    p.style.paddingLeft = '80px';
+    a.appendChild(p)
+    articleList.appendChild(a)
+}
